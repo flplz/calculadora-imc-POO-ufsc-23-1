1e00594d845951d5cae2ac380d7d5c62b99a31ef
Felipe Pacheco <lipe.fagundespacheco@gmail.com>
2023-05-09 17:07:47 -0300

    fix calculator

--- Calculadora/IMCCalculator.py
+++ Calculadora/IMCCalculator.py
@@ -7,12 +7,13 @@ class IMCCalculator:
         self.__title = "IMC Calculator"
 
     def buildWindow(self):
-        linha0= [sg.Text("Qual seu peso"), sg.InputText("",key ="weight"), sg.Text("Kg")]
-        linha1= [sg.Text("Qual sua altura"), sg.InputText("",key ="height"), sg.Text("cm")]
-        linha2= [sg.Text("Seu IMC é"), sg.InputText("",key ="imc", size=(6,1))]
-        linha3= [sg.Text("",size=(14,1)), sg.Button("CALCULAR IMC")]
-        container = [linha0,linha1,linha2,linha3]
-        return container
+        rows = [
+            [sg.Text("Qual seu peso"), sg.InputText("", key="weight"), sg.Text("Kg")],
+            [sg.Text("Qual sua altura"), sg.InputText("", key="height"), sg.Text("cm")],
+            [sg.Text("Seu IMC é"), sg.InputText("", key="imc", size=(6, 1))],
+            [sg.Text("", size=(14, 1)), sg.Button("CALCULAR IMC")]
+        ]
+        return rows
         
     def calculateIMC(self,values):
         heightStr = values["height"].replace(",","").replace(".","")
@@ -23,24 +24,24 @@ class IMCCalculator:
     
     
     def loop(self):
-        self.__window = sg.Window(self.__title, self.buildWindow(), font=("Helvetica", 14))
-        
-        try:
-            while self.__running:
-                event, values = self.__window.read()
-                
+        layout = [
+            [sg.Column(self.buildWindow(), element_justification="center")]
+        ]
+        self.__window = sg.Window(self.__title, layout, font=("Helvetica", 14))
+        while self.__running:
+            event, values = self.__window.read()
+            try:
                 if event == sg.WIN_CLOSED:
                     self.__running = False
                 elif event == "CALCULAR IMC":
                     imc = self.calculateIMC(values)
                     self.__window.Element("imc").Update(imc)
-        except ValueError:
-            sg.popup("Os valores de peso e altura devem ser números.")
-        except ZeroDivisionError:
-            sg.popup("A altura deve ser maior que zero.")
-        except Exception as e:
-            sg.popup(f"Ocorreu um erro: {e}")
-        finally:
-            self.__window.close()
+            except ValueError:
+                sg.popup("Os valores de peso e altura devem ser números.")
+            except ZeroDivisionError:
+                sg.popup("A altura deve ser maior que zero.")
+            except Exception as e:
+                sg.popup(f"Ocorreu um erro: {e}")
+        self.__window.close()
     
     
